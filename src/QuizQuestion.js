b144f57d902b644a498f4d2561993ecbaa215ff8
Pallavi Suryavanshi <pallavisuryavanshi633@gmail.com>
2020-05-04 12:38:13 +0530

    quizquitionV2

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,12 +5,15 @@ render(){
     return(
         <main>
         <section>
-    <p>{this.props.quiz_question[0]}</p>
+    <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className="buttons">
           <ul>
-            <li>{this.props.quiz_question[0]}</li>
-            
+            <li>{this.props.quiz_question.answer_options[0]}</li>
+            {/* <li>{this.props.quiz_question.answer_options[1]}</li>
+            <li>{this.props.quiz_question.answer_options[2]}</li>
+            <li>{this.props.quiz_question.answer_options[3]}</li>
+             */}
           </ul>
         </section>
       </main>
