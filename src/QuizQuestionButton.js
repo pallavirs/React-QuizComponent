af6fc7045c3f026e4074135760e380f462092ccb
Pallavi Suryavanshi <pallavisuryavanshi633@gmail.com>
2020-05-04 12:52:25 +0530

    quiz button

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class QuizQuestionButton extends Component{
+    render(){
+        return(
+              
+                <li><button>{this.props.button_text} </button></li>
+                // {/* <li>{this.props.quiz_question.answer_options[1]}</li>
+                // <li>{this.props.quiz_question.answer_options[2]}</li>
+                // <li>{this.props.quiz_question.answer_options[3]}</li>
+                //  */}
+        
+        )
+        }
+    }
+    
+    export default QuizQuestionButton;
