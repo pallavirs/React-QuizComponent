5fccc1a3c792c7b95194e3c45638e0a96a8aa4f3
Pallavi Suryavanshi <pallavisuryavanshi633@gmail.com>
2020-05-06 18:35:33 +0530

    'quizerromsg'

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,14 +10,14 @@ class Quiz extends Component{
         super(props);
         this.state = {quiz_position: 1};
     }
-    handleResetClick(){
-        this.setState({quiz_positionc:1})
-    }
     showNextQuestion(){
         this.setState((state)=> 
         {
             return {quiz_position: state.quiz_position + 1}
         })
+    }
+    handleResetClick(){
+        this.setState({quiz_position:1})
     }
 render(){
     const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
