021c26ff4cd235206b148bac6143ff7509da477c
Pallavi Suryavanshi <pallavisuryavanshi633@gmail.com>
2020-05-06 18:41:15 +0530

    'quizresetquiz'

--- src/Quiz.js
+++ src/Quiz.js
@@ -25,7 +25,7 @@ render(){
     
      return(
         <div>
-           {isQuizEnd ? <QuizEnd ResetClickHandler={this.handleResetClick.bind(this)}/> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
+           {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
         </div>
      )
     }
