1cfe73ad1fa001ce10047112e28881680a9ac6bc
Pallavi Suryavanshi <pallavisuryavanshi633@gmail.com>
2020-05-06 18:16:25 +0530

    'quizerromsg'

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import QuizQuestionButton  from './QuizQuestionButton.js';
 
 class QuizQuestion extends Component{
+  constructor(props){
+    super(props)
+    this.state={incorrectAnswer :false}
+  }
   handleClick(buttonText){
       if(buttonText === this.props.quiz_question.answer){
+        this.setState({incorrectAnswer :false})
         this.props.showNextQuestionHandler();
+      }else{
+        this.setState({incorrectAnswer :true})
       }
   }
 render(){
@@ -21,6 +28,7 @@ render(){
           }
           </ul>
         </section>
+        {this.state.incorrectAnswer ? <p className="error">sorry thats not correct answer</p>:null}
       </main>
     )
  }
